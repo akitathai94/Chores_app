c45002a921c8556fc1395192f5233f61a9bb4eca
pontusarfwedson <pontusarfwedson@gmail.com>
2016-09-15 01:43:24 -0700

    Searches for first and lastname

--- app/models/occupant.rb
+++ app/models/occupant.rb
@@ -3,7 +3,7 @@ class Occupant < ApplicationRecord
 
 def self.search(search)
   
-    where("firstname LIKE '%#{search}%'")
+    where("firstname LIKE '%#{search}%' OR lastname LIKE '%#{search}%'")
   
 end
 
